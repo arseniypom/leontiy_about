9b6f011df9647c116065f5bf9f6efab92e234d90
Arseniy Pomazkov <arpomazkov@bk.ru>
2023-08-11 18:25:18 +0300

    Updated site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./pages/**/*.html",
-    "./src/**/*.js",
+    './index.html',
+    './pages/**/*.html',
+    './src/**/*.js',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
+    colors: {
+      customBlue: '#01007f',
+      ytRed: '#ff0000',
+    },
+    fontFamily: {
+      shoika: ['Shoika', 'sistem-ui', 'sans-serif'],
+    },
   },
-  plugins: [],
-}
+  plugins: [require('flowbite/plugin')],
+};
